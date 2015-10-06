13ff8126eb5e2771e92bbd56d208ea8b5e61b19e
Gustaf <gustafbrunlof@gmail.com>
2015-10-06 16:26:08 +0200

    Möjligt att posta till db

--- client/app/addLocation/addLocation.controller.js
+++ client/app/addLocation/addLocation.controller.js
@@ -4,7 +4,7 @@ angular.module('campforfreeApp')
   .controller('AddLocationCtrl', function ($scope, $http) {
     $scope.message = 'Hello';
     $scope.addLoc = function() {
-      $http.post('/api/addLocation', { name: $scope.Name, longitude: $scope.Longitude, latitude: $scope.Latitude});
+      $http.post('/api/addLocations', { name: $scope.Name, longitude: $scope.Longitude, latitude: $scope.Latitude});
       $scope.Name = '';
       $scope.Longitude = '';
       $scope.Latitude = '';
