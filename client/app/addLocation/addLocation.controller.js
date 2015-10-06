d2db823ee143436816332bcb478de7f41f086d19
Gustaf <gustafbrunlof@gmail.com>
2015-10-06 17:45:09 +0200

    Lagt till en validering, lite css etc.

--- client/app/addLocation/addLocation.controller.js
+++ client/app/addLocation/addLocation.controller.js
@@ -1,13 +1,47 @@
 'use strict';
 
 angular.module('campforfreeApp')
-  .controller('AddLocationCtrl', function ($scope, $http) {
-    $scope.message = 'Hello';
+  .controller('AddLocationCtrl', function ($scope, $http, socket) {
+    $scope.locations = [];
+
+    $http.get('/api/addLocations').success(function(locations) {
+      $scope.locations = locations;
+      socket.syncUpdates('addLocation', $scope.locations);
+    });
+    
     $scope.addLoc = function() {
-      $http.post('/api/addLocations', { name: $scope.Name, longitude: $scope.Longitude, latitude: $scope.Latitude});
-      $scope.Name = '';
-      $scope.Longitude = '';
-      $scope.Latitude = '';
+      var validation = true;
+      var alertMessage = "";
+
+      if ($scope.Name === undefined && $scope.Longitude === undefined && $scope.Latitude === undefined) {
+      	alertMessage = 'Fyll i fälten fö faen';
+      	validation = false;
+      }
+      else if($scope.Name === undefined) {
+      	alertMessage = 'Fyll i namn';
+      	validation = false;
+      }
+      else if($scope.Latitude === undefined || isNaN($scope.Latitude)) {
+			alertMessage = 'Fyll i latitud(Siffra)';
+			validation = false;
+      }
+      else if($scope.Longitude === undefined || isNaN($scope.Longitude)) {
+	      	alertMessage = 'Fyll i longitud(Siffra)';
+	      	validation = false;
+	  }
+
+      if (alertMessage) {
+      	alert(alertMessage);
+      };
+
+      if (validation) {
+      	$http.post('/api/addLocations', { name: $scope.Name, longitude: $scope.Longitude, latitude: $scope.Latitude});
+	    $scope.Name = '';
+	    $scope.Longitude = '';
+	    $scope.Latitude = '';
+      };
+   	  
 
     };
+
   });
