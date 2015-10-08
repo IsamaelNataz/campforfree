b9f2310645be658082aa8b2f8c705562b9f61e3c
Gustaf <gustafbrunlof@gmail.com>
2015-10-08 15:56:24 +0200

    Lägga till pin

--- client/app/addLocation/addLocation.controller.js
+++ client/app/addLocation/addLocation.controller.js
@@ -2,18 +2,23 @@
 
 angular.module('campforfreeApp')
   .controller('AddLocationCtrl', function ($scope, $http, socket) {
+
     $scope.locations = [];
+    $scope.positions = [];
 
     $http.get('/api/addLocations').success(function(locations) {
       $scope.locations = locations;
       socket.syncUpdates('addLocation', $scope.locations);
+      // angular.forEach(locations, function (val) {
+      //   $scope.positions.push({lat: val.latitude, lng: val.longitude});
+      // });
     });
-    
+
     $scope.addLoc = function() {
       var validation = true;
       var alertMessage = "";
 
-      if ($scope.Name === undefined && $scope.Longitude === undefined && $scope.Latitude === undefined) {
+      if ($scope.Name === undefined) {
       	alertMessage = 'Fyll i fälten fö faen';
       	validation = false;
       }
@@ -21,30 +26,32 @@ angular.module('campforfreeApp')
       	alertMessage = 'Fyll i namn';
       	validation = false;
       }
-      else if($scope.Latitude === undefined || isNaN($scope.Latitude)) {
-			alertMessage = 'Fyll i latitud(Siffra)';
-			validation = false;
-      }
-      else if($scope.Longitude === undefined || isNaN($scope.Longitude)) {
-	      	alertMessage = 'Fyll i longitud(Siffra)';
-	      	validation = false;
-	  }
 
-      if (alertMessage) {
+      if (alertMessage) { 
       	alert(alertMessage);
       };
 
       if (validation) {
-      	$http.post('/api/addLocations', { name: $scope.Name, longitude: $scope.Longitude, latitude: $scope.Latitude});
-	    $scope.Name = '';
-	    $scope.Longitude = '';
-	    $scope.Latitude = '';
-      };  
+        var lati = $scope.positions[0].lat;
+        var longi = $scope.positions[0].lng;
+		    $http.post('/api/addLocations', { name: $scope.Name, longitude: lati, latitude: longi});
+	    	
+        $scope.Name = '';
+		    $scope.Longitude = '';
+		    $scope.Latitude = '';
+		}
 
     };
 
     $scope.deleteLocation = function(location) {
-      $http.delete('/api/addLocations/' + location._id);
+      	$http.delete('/api/addLocations/' + location._id);
     };
 
-  });
+	  $scope.addMarker = function(event) {
+	    
+      var ll = event.latLng;
+      $scope.positions.push({lat:ll.lat(), lng: ll.lng()});
+      //console.log(ll.lat(), ll.lng());
+      //$http.post('/api/addLocations', { name: '', longitude: ll.lat(), latitude: ll.lng()});
+	  }
+});
