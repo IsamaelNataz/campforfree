c2a883ecaf7c9f8340ba3c0392830e37724ee937
Gustaf <gustafbrunlof@gmail.com>
2015-11-17 22:01:33 +0100

    Css, admin bara ser userrole

--- server/auth/local/passport.js
+++ server/auth/local/passport.js
@@ -13,10 +13,10 @@ exports.setup = function (User, config) {
         if (err) return done(err);
 
         if (!user) {
-          return done(null, false, { message: 'This email is not registered.' });
+          return done(null, false, { message: 'Den här e-mailen är ej registrerad.' });
         }
         if (!user.authenticate(password)) {
-          return done(null, false, { message: 'This password is not correct.' });
+          return done(null, false, { message: 'Felaktigt lösenord.' });
         }
         return done(null, user);
       });
